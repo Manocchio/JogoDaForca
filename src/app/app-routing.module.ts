96392f96ae7dfdff535e60db0424b4a7f564b738
Gustavo Nunes <urameshi.gustavo668@gmail.com>
2019-11-11 16:08:09 -0300

    criação da pasta sobre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'game', loadChildren: './game/game.module#GamePageModule' },
+  { path: 'sobre', loadChildren: './sobre/sobre.module#SobrePageModule' },
 ];
 
 @NgModule({
