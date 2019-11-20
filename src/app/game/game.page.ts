60a859897f4228b5d12f6308fe0ee1300cf3a382
miguelrisquelme <miguelnascimento22@hotmail.com>
2019-11-20 02:35:57 -0200

    Feita as dicas de ajuda do jogo

--- src/app/game/game.page.ts
+++ src/app/game/game.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { NavController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-game',
@@ -8,18 +8,70 @@ import { NavController } from '@ionic/angular';
 })
 export class GamePage implements OnInit {
 
-  public jogos: string[] = ["God of War",
-    "Resident evil", "Shadow of the colossus", "Crash", "Black", "Megaman",
-    "Final Fantasy", "Devil may cry", "Space invanders", "Pitfall", "Left four dead",
-    "Castlevania", "Metal gear solid", "Hitman", "Call of duty", "Dino Crisis",
-    "The legend of zelda", "Super smash bros", "Spyro", "Dark Souls"];
+  public jogos: string[] =  [ "God of War",
+                              "Resident evil", 
+                              "Shadow of the colossus", 
+                              "Crash", 
+                              "Black", 
+                              "Megaman",
+                              "Final Fantasy", 
+                              "Devil may cry", 
+                              "Space invanders", 
+                              "Pitfall", 
+                              "Left four dead",
+                              "Castlevania", 
+                              "Metal gear solid", 
+                              "Hitman", 
+                              "Call of duty",
+                              "Dino Crisis",
+                              "The legend of zelda", 
+                              "Super smash bros", 
+                              "Spyro", 
+                              "Dark Souls"
+                            ];
+
+  
 
   public palavraSorteada: string;
   public vidasRestantes: number = 6;
   public quantidadeAcertos: number = 0;
   public letrasUsadas: string[] = [];
+  public dicas =  [ { dica:"Um cara com tatuagem vermelha, conhecido como fantasma de sparta.", nome:"God of War" },
+                    { dica:"Apocalipse zumbi, e protagonista de cabelo lambido", nome:"Resident evil"},
+                    { dica:"Um cara em um cavalinho caçando titans", nome:"Shadow of the colossus"},
+                    { dica:"Uma raposa aleatória pulando em caixas", nome:"Crash"},
+                    { dica:"Melhor jogo de tiro do PS2", nome:"Black"},
+                    { dica:"Jogo de plataforma com um robô azul", nome:"Megaman"},
+                    { dica:"Joguinho de fantasia, um dos mais conhecidos RPGs já feitos", nome:"Final Fantasy"},
+                    { dica:"Um demonio que pode chorar", nome:"Devil may cry"},
+                    { dica:"Primeiro joguinho de nave, coisa de Atari", nome:"Space invaders"},
+                    { dica:"O Indiana Jones do Atari", nome:"Pitfall"},
+                    { dica:"Jogo de zumbi da Valve", nome:"Left four dead"},
+                    { dica:"Um jogo para caçar vampiros e dar chicotadas", nome:"Castlevania"},
+                    { dica:"Um jogo de espionagem, tática e furtividade", nome:"Metal gear solid"},
+                    {, nome:},
+                    {, nome:},
+                    {, nome:},
+                    {, nome:},
+                    {, nome:},
+                    {, nome:},
+                    
+                  ];
 
-  constructor(public nav: NavController) { }
+  constructor(public nav: NavController, public alertController: AlertController) { }
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+  
 
   ngOnInit() {
     this.start();
@@ -32,7 +84,7 @@ export class GamePage implements OnInit {
 
   public start(): void {
 
-
+    console.log(""+ this.dicas[0].dica);
     var indiceSelecionado = this.getRandom(this.jogos.length - 1);
     this.palavraSorteada = this.jogos[indiceSelecionado];
 
@@ -66,6 +118,8 @@ export class GamePage implements OnInit {
     console.log(this.palavraSorteada);
 
   }
+
+
 
 
   public verificarLetra(letra: string): void {
